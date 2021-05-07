c7fb96fe71a9c455402950a522e4710fc149bb3d
Aleksi Jokinen <aleksijokinen@outlook.com>
2021-05-07 18:27:46 +0300

    Major edits to the source. Test iterates now for 32,767 cycles and ends. Refactored also formatting of the source.

--- IoT/ESP8266/Tester/Tester.ino
+++ IoT/ESP8266/Tester/Tester.ino
@@ -1,28 +1,30 @@
-    /* Tester for the NodeMCU microcontroller.
-     * This tool is made to see that basic functionality and boards work. 
-     * 
-     * Developed by Aleksi Jokinen 2021
-     */    
-     int n = 0;
+/* Tester for the NodeMCU microcontroller.
+ * This tool is made to see that basic functionality and boards work. 
+ * 
+ * Developed by Aleksi Jokinen 2021
+*/    
+int n = 0;
      
-     void setup() {
-      //Delays added to properly setup the program.
-      Serial.begin(9600);
-      delay(1000);
-      Serial.println("ESP8266, NodeMCU test");
-      delay(100);
-      Serial.println("Starting to iterate n(1)");
-      delay(100);
-      Serial.println("n, integer, maximum value 32,767");
-      delay(100);
-      Serial.println("Running at 9600bps");
-      delay(100);
-      Serial.println("Loop delay 100ms");
-      delay(2000);
-     }
+void setup() {
+//Delays added to properly setup the program.
+Serial.begin(9600);  //Baud rate needs to be 9600 or NodeMCU doesn't work correctly.
+Serial.println("ESP8266, NodeMCU test");
+Serial.println("Starting to iterate n(1)");
+Serial.println("n, integer, maximum value 32,767");
+Serial.println("Running at 9600bps");
+Serial.println("Loop delay 100ms");
+}
 
-     void loop() {
-      n++;
-      Serial.println(n);
-      delay(100); 
-     }
+//Iteration tester. 
+void loop() {
+  if(n < 32768) {
+    n++;
+    Serial.println(n);
+  }
+  else {
+    Serial.println("Iteration test finished with 32,767 cycles");
+    Serial.println("Test passed.");
+    return;
+  }
+  delay(100);
+}
