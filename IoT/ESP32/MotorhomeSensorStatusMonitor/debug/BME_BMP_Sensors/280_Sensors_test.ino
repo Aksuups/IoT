02393cc0fdb12d0b268b11b20f6800aa1382d07d
Aleksi Jokinen <aleksijokinen@outlook.com>
2021-08-29 19:27:11 +0300

    Software now measures BME280 and BMP280 sensor data. Output to serial is formatted. Example output: [Starting] Opening the serial port - COM3 BME280 and BMP280 sensor testing tool. Software version: 1.0 Developed by Aleksi Jokinen © 2021 ----------------------------------------- Test cycle [1] Sensor:           BME280          BMP280 Address:            0x60            0x58 Temperature:       25.15           25.11 Humidity:          51.10           --.-- Pressure:        1003.46         1003.35 ----------------------------------------- Test cycle [2] Sensor:           BME280          BMP280 Address:            0x60            0x58 Temperature:       25.18           25.12 Humidity:          50.50           --.-- Pressure:        1003.47         1003.40 ----------------------------------------- Test cycle [3] Sensor:           BME280          BMP280 Address:            0x60            0x58 Temperature:       25.20           25.12 Humidity:          50.25           --.-- Pressure:        1003.45         1003.41 -----------------------------------------

--- IoT/ESP32/MotorhomeSensorStatusMonitor/debug/BME_BMP_Sensors/280_Sensors_test.ino
+++ IoT/ESP32/MotorhomeSensorStatusMonitor/debug/BME_BMP_Sensors/280_Sensors_test.ino
@@ -1,3 +1,10 @@
+/* ------------------------------------------------------------------------ *
+ *  Testing tool for BME280 and BMP280 sensors.                             *
+ *                                                                          *
+ *  ----------------------------------------------------------------------- *
+ *  Developed by Aleksi Jokinen © 2021                                      *
+ *  https://www.github.com/Aksuups                                          *
+ * ------------------------------------------------------------------------ */
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BME280.h>
@@ -6,11 +13,14 @@
 
 Adafruit_BME280 bme;
 Adafruit_BMP280 bmp;
-Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
-Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+
+int counter = 1;
 
 void setup(){
   Serial.begin(9600);
+  Serial.println("BME280 and BMP280 sensor testing tool.\n");
+  Serial.println("Software version: 1.0");
+  Serial.println("Developed by Aleksi Jokinen © 2021\n");
 
   if (!bme.begin(0x76)) {
     Serial.println("BME280-sensor not found.");
@@ -18,37 +28,44 @@ void setup(){
     Serial.println(bme.sensorID(),16);
     while(1);
     }
+
   if (!bmp.begin(0x77)) {
     Serial.println("BMP280-sensor not found.");
     Serial.print("Sensor address: ");
     Serial.println(bmp.sensorID(),16);
     while(1);
     }
-
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-
-  bmp_temp->printSensorDetails();
 }
 
-void loop() {
-  sensors_event_t temp_event, pressure_event;
-  bmp_temp->getEvent(&temp_event);
-  bmp_pressure->getEvent(&pressure_event);
-  
-  Serial.println("Sensor: BMP280");
-  Serial.print(F("Temperature = "));
-  Serial.print(temp_event.temperature);
-  Serial.println(" *C");
+void Test(){
+    Serial.println("Sensor:           BME280          BMP280");
+    Serial.print("Address:            0x");
+    Serial.print(bme.sensorID(), 16);
+    Serial.print("            0x");
+    Serial.println(bmp.sensorID(), 16);
+    Serial.print("Temperature:       ");
+    Serial.print(bme.readTemperature());
+    Serial.print("           ");
+    Serial.println(bmp.readTemperature() - 1.15); // Calibration value to match BME280 within one decimal place.
+    Serial.print("Humidity:          ");
+    Serial.print(bme.readHumidity());
+    Serial.print("           ");
+    Serial.println("--.--");
+    Serial.print("Pressure:        ");
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.print("         ");
+    Serial.println(bmp.readPressure() / 100.0F - 0.85); // Calibration value to match BME280 within one decimal place.
+}
 
-  Serial.print(F("Pressure = "));
-  Serial.print(pressure_event.pressure);
-  Serial.println(" hPa");
-  Serial.println();
-  Serial.println();
-  delay(10000);
+
+
+void loop() {
+    Serial.println("-----------------------------------------");
+    Serial.print("Test cycle [");
+    Serial.print(counter);
+    Serial.println("]");
+
+    Test();
+    counter++;
+    delay(20000);
 }
